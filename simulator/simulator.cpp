95ee4e1c3c68c5bed08f11c30d2ba68294dcb2fb
System Administrator <root@o197-181.pubnet.titech.ac.jp>
2012-07-13 21:52:27 +0900

    map

---
+++ simulator/simulator.cpp
@@ -0,0 +1,31 @@
+#include<cstdio>
+#include<cstdlib>
+#include<cmath>
+#include<climits>
+#include<iostream>
+#include<algorithm>
+#include<complex>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<set>
+#include<utility>
+#include<map>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> PII;
+
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define REP(i,n) FOR(i,0,n)
+#define MP make_pair
+
+int main(){
+
+
+  return 0;
+}
+
