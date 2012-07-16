8dcaba099f57f5e453ad3a94d9fd61c91bd3f3c0
draftcode <draftcode@gmail.com>
2012-07-16 11:12:25 +0900

    Add 2nd deadline.

--- batch-check/batch-check.py
+++ batch-check/batch-check.py
@@ -16,6 +16,7 @@ import cPickle as pickle
 base_path = os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0])))
 
 def get_point(commands, map_path):
+    start_time = time.time()
     p = subprocess.Popen(['ruby',
                           os.path.join(base_path, "simulator", "Ruby", "main.rb"),
                           map_path,
@@ -25,7 +26,10 @@ def get_point(commands, map_path):
                          stderr=subprocess.PIPE)
     p.stdin.write(commands)
     p.stdin.close()
-    p.wait()
+    while p.poll() is None:
+        if time.time() - start_time >= 5:
+            raise Exception
+        time.sleep(0.1)
 
     result = p.stdout.read()
     match = re.search("Score: (\d+)", result)
@@ -58,14 +62,30 @@ def get_command(lifter, map_path, timeout=150):
                 p.send_signal(signal.SIGINT)
                 break
             time.sleep(0.1)
-    except KeyboardInterrupt as e:
+    except KeyboardInterrupt:
         p.send_signal(signal.SIGINT)
-    p.wait()
+
+    while p.poll() is None:
+        try:
+            out += p.stdout.read()
+            sp = out.split()
+            if len(sp) != 0:
+                out = sp[-1]
+        except:
+            pass
+        if time.time() - start_time >= timeout + 10:
+            p.kill()
+            raise Exception
+        time.sleep(0.1)
+
     elapsed_time = time.time() - start_time
-    out += p.stdout.read()
-    sp = out.split()
-    if len(sp) != 0:
-        out = sp[-1]
+    try:
+        out += p.stdout.read()
+        sp = out.split()
+        if len(sp) != 0:
+            out = sp[-1]
+    except:
+        pass
 
     return (out, elapsed_time)
 
@@ -88,24 +108,32 @@ def main():
     for map_path in files:
         if args.pattern and not re.search(args.pattern, map_path):
             continue
+        key = os.path.basename(map_path)
+        print "%s\t" % key,
+        sys.stdout.flush()
         try:
-            key = os.path.basename(map_path)
-            print "%s\t" % key,
-            sys.stdout.flush()
             command, elapsed_time = get_command(lifter, map_path, args.timeout)
-            point = get_point(command, map_path)
-            if key in results:
-                prev_point, prev_time = results[key]
-                print "%d\t(%fsec)\t(%+dpoint, %+fsec)" % (
-                        point, elapsed_time, point-prev_point, elapsed_time-prev_time)
-            else:
-                print "%d\t(%fsec)" % (point, elapsed_time)
-            print command
-            results[key] = (point, elapsed_time)
-            pickle.dump(results, open(results_path, 'wb'))
-        except KeyboardInterrupt:
+        except Exception:
+            print "lifter does not respond"
             print ""
-            pass
+            continue
+
+        try:
+            point = get_point(command, map_path)
+        except Exception:
+            print "simulator does not respond"
+            print command
+            continue
+
+        if key in results:
+            prev_point, prev_time = results[key]
+            print "%d\t(%fsec)\t(%+dpoint, %+fsec)" % (
+                    point, elapsed_time, point-prev_point, elapsed_time-prev_time)
+        else:
+            print "%d\t(%fsec)" % (point, elapsed_time)
+        print command
+        results[key] = (point, elapsed_time)
+        pickle.dump(results, open(results_path, 'wb'))
 
 if __name__ == '__main__':
     main()
